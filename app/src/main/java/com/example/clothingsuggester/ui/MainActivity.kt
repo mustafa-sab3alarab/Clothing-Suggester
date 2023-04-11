82b8e292c6f1a826de5ff60f1dd8896e34fb534c
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-04-11 06:08:13 +0200

    complete main activity design

--- app/src/main/java/com/example/clothingsuggester/ui/MainActivity.kt
+++ app/src/main/java/com/example/clothingsuggester/ui/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.clothingsuggester.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.clothingsuggester.R
+import com.example.clothingsuggester.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
