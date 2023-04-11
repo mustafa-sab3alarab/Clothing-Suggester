9c81967c925ff35d93d99cc948a62a76ef1b57f0
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-04-11 05:51:01 +0200

    complete splash design

--- app/src/main/java/com/example/clothingsuggester/ui/MainActivity.kt
+++ app/src/main/java/com/example/clothingsuggester/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.clothingsuggester
+package com.example.clothingsuggester.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.clothingsuggester.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
