d2eb92d783ca3f66f79efc126321a17b9d2b5bcf
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-04-11 18:25:24 +0200

    connect api with presenter

--- app/src/main/java/com/example/clothingsuggester/presenter/HomePresenter.kt
+++ app/src/main/java/com/example/clothingsuggester/presenter/HomePresenter.kt
@@ -1,4 +1,10 @@
 package com.example.clothingsuggester.presenter
 
-class HomePresenter {
+import com.example.clothingsuggester.model.ApiServiceImp
+import com.example.clothingsuggester.ui.home.IHomeView
+
+class HomePresenter(val homeView : IHomeView) {
+
+    private val apiService = ApiServiceImp()
+
 }
--- app/src/main/java/com/example/clothingsuggester/presenter/SplashPresenter.kt
+++ app/src/main/java/com/example/clothingsuggester/presenter/SplashPresenter.kt
@@ -1,4 +1,30 @@
 package com.example.clothingsuggester.presenter
 
-class SplashPresenter {
+import com.example.clothingsuggester.model.ApiServiceImp
+import com.example.clothingsuggester.ui.splash.ISplashView
+import com.google.android.gms.maps.model.LatLng
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
+
+class SplashPresenter(val splashView: ISplashView) {
+
+    private val apiService = ApiServiceImp()
+
+    fun getCurrentWeatherMap(
+        latLng: LatLng,
+        onResponse: (call: Call, response: Response) -> Unit,
+        onFailure: (call: Call, e: IOException) -> Unit
+    ) {
+        apiService.getWeatherMap(latLng).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                onFailure(call, e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                onResponse(call, response)
+            }
+        })
+    }
 }
