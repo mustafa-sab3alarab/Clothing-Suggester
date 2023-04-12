01f0e9a176bd284c0d6759b539d7e5195b33b4d8
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-04-12 18:40:46 +0200

    create db to provide weather image

---
+++ app/src/main/java/com/example/clothingsuggester/util/WeatherIconsConstants.kt
@@ -0,0 +1,22 @@
+package com.example.clothingsuggester.util
+
+enum class WeatherIconsConstants(val iconId: String) {
+    CLEAR_SKY_DAY("01d"),
+    CLEAR_SKY_NIGHT("01n"),
+    FEW_CLOUDS_DAY("02d"),
+    FEW_CLOUDS_NIGHT("02n"),
+    SCATTERED_CLOUDS_DAY("03d"),
+    SCATTERED_CLOUDS_NIGHT("03n"),
+    BROKEN_CLOUDS_DAY("04d"),
+    BROKEN_CLOUDS_NIGHT("04n"),
+    SHOWER_RAIN_DAY("09d"),
+    SHOWER_RAIN_NIGHT("09n"),
+    RAIN_DAY("10d"),
+    RAIN_NIGHT("10n"),
+    THUNDERSTORM_DAY("11d"),
+    THUNDERSTORM_NIGHT("11n"),
+    SNOW_DAY("13d"),
+    SNOW_NIGHT("13n"),
+    MIST_DAY("50d"),
+    MIST_NIGHT("50n")
+}
