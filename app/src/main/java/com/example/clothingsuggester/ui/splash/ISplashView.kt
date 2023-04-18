bd1abb09dd650177572cd679eb0a87793f1ef078
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-04-18 15:02:29 +0200

    rename function to what it does

--- app/src/main/java/com/example/clothingsuggester/ui/splash/ISplashView.kt
+++ app/src/main/java/com/example/clothingsuggester/ui/splash/ISplashView.kt
@@ -3,6 +3,6 @@ package com.example.clothingsuggester.ui.splash
 import com.example.clothingsuggester.model.domain.WeatherMap
 
 interface ISplashView {
-    fun onSuccessWeather(weatherMap : WeatherMap)
-    fun onFailureWeather(error : String)
+    fun navigateToHome(weatherMap : WeatherMap)
+    fun showSnackbarError(error : String)
 }
