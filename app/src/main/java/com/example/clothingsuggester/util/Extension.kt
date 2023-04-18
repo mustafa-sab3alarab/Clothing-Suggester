9f6c35160729e8aef7569bc65d422651457ff63b
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-04-18 14:58:22 +0200

    create function that return next day

--- app/src/main/java/com/example/clothingsuggester/util/Extension.kt
+++ app/src/main/java/com/example/clothingsuggester/util/Extension.kt
@@ -2,6 +2,7 @@ package com.example.clothingsuggester.util
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import java.util.Calendar
 
 fun String.toCelsius(): String {
     return "${(this.toDouble() - 273.15).toInt()}"
@@ -13,4 +14,14 @@ fun String.toKmPerHour(): String {
 
 fun ImageView.loadImage(url: String) {
     Glide.with(context).load(url).centerCrop().into(this)
+}
+
+fun getCurrentDay(): String {
+    val calendar = Calendar.getInstance()
+    return calendar.get(Calendar.DAY_OF_MONTH).toString()
+}
+
+fun getNextDay(): String {
+    val calendar = Calendar.getInstance()
+    return calendar.add(Calendar.DAY_OF_YEAR, 1).toString()
 }
